38fdbc7bc59f6cdf779d1d539d6053df261a6b4c
Huy Nguyen <huy@ronin.ws>
2016-02-12 23:55:22 -0800

    removed dead comment code

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -33,19 +33,6 @@ namespace Tests
 
 		private static void RunParallel(int count, string url)
         {
-			//var proxy = new SocksWebProxy(new ProxyConfig(
-			//    //This is an internal http->socks proxy that runs in process
-			//    IPAddress.Parse("127.0.0.1"),
-			//    //This is the port your in process http->socks proxy will run on
-			//    12345,
-			//    //This could be an address to a local socks proxy (ex: Tor / Tor Browser, If Tor is running it will be on 127.0.0.1)
-			//    IPAddress.Parse("127.0.0.1"),
-			//    //This is the port that the socks proxy lives on (ex: Tor / Tor Browser, Tor is 9150)
-			//    9150,
-			//    //This Can be Socks4 or Socks5
-			//    ProxyConfig.SocksVersion.Five
-			//    ));
-
 			var proxy = new SocksWebProxy();
 			Enumerable.Range(0, count).ToList().ForEach(new Action<int>(x =>
             {
